8ad9a2ce8efce781369fb8d2982b9c749fe9f504
岚海网络 <lanhaiwangluo@lanhaiwangluodemac-mini.local>
2015-12-24 16:36:16 +0800

    Initial Commit

---
+++ YLSideLipView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YLSideLipView
+//
+//  Created by 岚海网络 on 15/12/24.
+//  Copyright © 2015年 岚海网络. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
